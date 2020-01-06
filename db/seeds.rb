0e16e1457c311631e5cefd15f7141e549f26c4b2
Marc Seitz <marc@mfts.io>
2020-01-06 16:43:46 +0200

    update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(name: 'Marc', email: 'marc@mfts.io')
+Location.create(city: 'Helsinki', country: 'Finland', location_key: 133328)
